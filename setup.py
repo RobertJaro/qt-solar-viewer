f9ded531da54be2a2754c4f54e6b7637ae3cc3cf
Robert Jarolim <robert.jarolim@outlook.com>
2018-04-15 11:57:00 +0200

    initial commit

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='solarviewer',
+    version='0.1',
+    description='Viewer application for solar physics',
+    author='Robert Jarolim',
+    packages=find_packages(),
+    provides=find_packages(),
+    install_requires=['sunpy==0.8.5', 'qtpy', 'pywavelets', 'scikit-image'],
+    python_requires='>=3',
+    package_data={'solarviewer.resource': ['*.png', '*.txt', '*.pkl']},
+    entry_points={
+        'gui_scripts': ['solarviewer=solarviewer.main:main'],
+    }
+)
